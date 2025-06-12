fbcc4ddc7344ddf6c07fc51aeed17e5b8ebb412a
owenisas <owenisas@gmail.com>
2025-06-12 02:43:28 -0700

    Initial Commit

---
+++ Test_Foundation_App/Test_Foundation_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_Foundation_AppApp.swift
+//  Test_Foundation_App
+//
+//  Created by user on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Test_Foundation_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Test_Foundation_AppTests/Test_Foundation_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Test_Foundation_AppTests.swift
+//  Test_Foundation_AppTests
+//
+//  Created by user on 6/12/25.
+//
+
+import Testing
+@testable import Test_Foundation_App
+
+struct Test_Foundation_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
